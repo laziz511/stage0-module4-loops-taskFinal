c59a8e8892e98573777377d6f43c6ab3eb61efdc
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-24 11:37:48 +0500

    fixed

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i=0; i<cathetusLength; i++){
+            for (int j=0; j<cathetusLength*2; j++){
+                if (i+j>=cathetusLength && j-i<=cathetusLength){
+                    System.out.print(Math.abs(j-cathetusLength)+1);
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
     }
 
